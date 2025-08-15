189f21b25bcbc49973ee98fbcbe3dd309a219ffa
Yamada <85409185+DevYamada@users.noreply.github.com>
2025-08-15 08:38:51 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using hospedagem.Models;
+using Hospedagem.Models;
+
+List<Hospede> hospedes = new List<Hospede>();
+
+Hospede p1 = new Hospede(nomeCompleto: "Joas asd");
+Hospede p2 = new Hospede(nomeCompleto: "kas aso");
+
+hospedes.Add(p1);
+hospedes.Add(p2);
+
+
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 3, valor: 30);
+
+
+Reserva reserva = new Reserva(DiasReservados: 5);
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospedes);
+
+
+Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor: {reserva.ObterValor()}");
